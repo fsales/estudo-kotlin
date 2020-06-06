dd1aa0bfdc58abeac5d0e4a178a4eb1027a4eb3a
Fábio Sales <fabio.oliveira.sales@gmail.com>
2020-06-06 20:13:00 -0300

    assessores customizados

---
+++ PrimeiroPrograma/src/When.kt
@@ -0,0 +1,21 @@
+fun obterMnemonica(cor: Cor) =
+    when (cor) {
+        Cor.VERMELHO -> "Ver"
+        Cor.AMARELO -> "Alguém"
+        Cor.VERDE -> "Viver"
+        Cor.AZUL -> "Além é"
+        Cor.LARANJA -> "Legal"
+    }
+
+fun obterTemperatura(cor: Cor) : String{
+
+    return when(cor){
+        Cor.VERMELHO, Cor.LARANJA, Cor.AMARELO -> "Quente"
+        Cor.VERDE -> "Neutro"
+        Cor.AZUL -> "Frio"
+    }
+}
+
+fun main() {
+    println(obterMnemonica(Cor.LARANJA))
+}
---
+++ PrimeiroPrograma/src/br/com/fsales/pacotea/PacoteA.kt
@@ -0,0 +1,3 @@
+package br.com.fsales.pacotea
+
+fun nome() = "Pacote A"
