4e93a00b66a00d82c7fcd895f35d27436c83d744
Fábio Sales <fabio.oliveira.sales@gmail.com>
2020-06-06 20:54:09 -0300

    for

--- PrimeiroPrograma/src/FizzBuzz.kt
+++ PrimeiroPrograma/src/FizzBuzz.kt
@@ -1,4 +1,6 @@
-fun fizBuzz(numero:Int) = when {
+import java.util.*
+
+fun fizBuzz(numero: Int) = when {
     numero % 15 == 0 -> "FizzBuzz"
     numero % 3 == 0 -> "Fizz"
     numero % 5 == 0 -> "Buzz"
@@ -6,8 +8,22 @@ fun fizBuzz(numero:Int) = when {
 }
 
 fun main() {
-    for (numero in 1..100){
+    for (numero in 1..100) {
         val resul = fizBuzz(numero)
         println(resul)
     }
+
+
+    //estrutura de dados map
+    val binaryRepresentation = TreeMap<Char, String>()
+
+    for (c in 'A'..'F') {
+        val binary = Integer.toBinaryString(c.toInt())
+        binaryRepresentation[c] = binary
+    }
+
+    for((letter, binary) in binaryRepresentation){
+        println("$letter - $binary")
+    }
+
 }
